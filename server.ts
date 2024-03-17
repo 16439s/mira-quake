238bf7b096fa7f286539e44f2ddf7c967ed36657
ありす <master@16439s.jp>
2024-03-18 08:41:28 +0900

    Feat(Fix): Miraの検知能力向上

--- server.ts
+++ server.ts
@@ -1,24 +1,18 @@
 import https from 'https';
 import dotenv from 'dotenv';
+
+// .envファイルからトークンを読み込む
 dotenv.config();
+const MISSKEY_API_TOKEN = process.env.MISSKEY_API_TOKEN;
 
 // p2pquakeのAPIエンドポイント
 const P2PQUAKE_API_ENDPOINT = 'https://api.p2pquake.net/v2/history?codes=551&limit=1';
 
-// MisskeyのAPIエンドポイントとトークン
+// MisskeyのAPIエンドポイント
 const MISSKEY_API_ENDPOINT = 'https://freelynetwork.jp/api/notes/create';
-const MISSKEY_API_TOKEN = process.env.MISSKEY_API_TOKEN;
 
-// 前回の地震情報を保持する変数
+// 前回の地震情報
 let previousEarthquakeData: any = null;
-
-// 定期的に地震情報を取得して処理する関数
-function fetchAndProcessEarthquakeData() {
-    fetchEarthquakeData(); // 初回呼び出し
-
-    // 1分ごとに地震情報を取得し処理する
-    setInterval(fetchEarthquakeData, 60000);
-}
 
 // HTTPリクエストを送信して地震データを取得する関数
 function fetchEarthquakeData() {
@@ -36,11 +30,10 @@ function fetchEarthquakeData() {
                 const earthquakeData = JSON.parse(data)[0];
                 console.log('Received earthquake data:', earthquakeData);
 
-                // 前回の地震情報と比較して変更があるか確認
-                if (!isEqual(earthquakeData, previousEarthquakeData)) {
-                    // 地震情報が変更された場合のみMisskeyに投稿
+                // 地震情報が更新された場合のみMisskeyに投稿
+                if (!isEqual(earthquakeData)) {
                     postToMisskey(earthquakeData);
-                    // 変更された地震情報を保存
+                    // 更新された地震情報を保存
                     previousEarthquakeData = earthquakeData;
                 } else {
                     console.log('No changes in earthquake data, skipping post');
@@ -55,9 +48,17 @@ function fetchEarthquakeData() {
 }
 
 // 地震情報の比較関数
-function isEqual(earthquake1: any, earthquake2: any): boolean {
-    // 地震情報のJSON文字列を比較
-    return JSON.stringify(earthquake1) === JSON.stringify(earthquake2);
+function isEqual(newData: any): boolean {
+    if (!previousEarthquakeData) {
+        return false;
+    }
+    // 特定のフィールドの値を比較
+    return (
+        newData.earthquake.time === previousEarthquakeData.earthquake.time &&
+        newData.earthquake.hypocenter.name === previousEarthquakeData.earthquake.hypocenter.name &&
+        newData.earthquake.hypocenter.magnitude === previousEarthquakeData.earthquake.hypocenter.magnitude &&
+        newData.time === previousEarthquakeData.time
+    );
 }
 
 // Misskeyに地震情報を投稿する関数
@@ -99,5 +100,5 @@ function postToMisskey(earthquakeData: any) {
     });
 }
 
-// 地震情報の定期的な取得と処理を開始する
-fetchAndProcessEarthquakeData();
+// 1分ごとに地震データを取得して処理する
+setInterval(fetchEarthquakeData, 60000);
